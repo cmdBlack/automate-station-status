3190b86a9866d2496f74d68693989b7af17bbcbe
Kaizer Macni <132550759+cmdBlack@users.noreply.github.com>
2024-10-03 09:41:02 +0800

    Add files via upload

---
+++ station-status.py
@@ -0,0 +1,191 @@
+#VARIABLES
+
+#date = "03-10-2024"
+
+rr_vigan = 4.0
+rr_bantay = 2.5
+rr_dolores = 0
+rr_luba = 0
+
+#RISING
+#RECEDING
+#NSC
+
+wl_bantay = "RECEDING"
+wl_lapaz = "RECEDING"
+wl_dolores = "RECEDING"
+
+
+
+
+
+
+
+
+import os
+
+from qgis.core import (
+    QgsGeometry,
+    QgsMapSettings,
+    QgsPrintLayout,
+    QgsMapSettings,
+    QgsMapRendererParallelJob,
+    QgsLayoutItemLabel,
+    QgsLayoutItemLegend,
+    QgsLayoutItemMap,
+    QgsLayoutItemPolygon,
+    QgsLayoutItemScaleBar,
+    QgsLayoutExporter,
+    QgsLayoutItem,
+    QgsLayoutPoint,
+    QgsLayoutSize,
+    QgsUnitTypes,
+    QgsProject,
+    QgsFillSymbol,
+    QgsAbstractValidityCheck,
+    check,
+)
+
+from qgis.PyQt.QtGui import (
+    QPolygonF,
+    QColor,
+)
+
+from qgis.PyQt.QtCore import (
+    QPointF,
+    QRectF,
+    QSize,
+)
+
+from datetime import date
+
+today = date.today()
+
+project = QgsProject.instance()
+            
+manager = project.layoutManager()
+layoutName = "RG-WL_STAT"
+layouts_list = manager.printLayouts()
+
+
+
+
+layout = QgsPrintLayout(project)
+layout.initializeDefaults()
+
+        
+document = QDomDocument()
+
+# read template content
+template_file = open('C:\\Users\\User\\Documents\\KAI FILES\\AbRBFFWC OBSERVER DIRECTORY\\ABRA BASIN DIRECTORY\\ABRA BASIN DIRECTORY\\DAILY REPORT\\WL change monitoring\\WL_RR.qpt')
+template_content = template_file.read()
+template_file.close()
+document.setContent(template_content)
+
+# Remove any duplicate layouts
+for i in layouts_list:
+    if i.name() == layoutName:
+        manager.removeLayout(i)
+
+# load layout from template and add to Layout Manager
+layout.loadFromTemplate(document, QgsReadWriteContext()) 
+project.layoutManager().addLayout(layout)
+
+#STATIC VARIABLES
+n_rains = "C:/Users/User/Documents/KAI FILES/AbRBFFWC OBSERVER DIRECTORY/ABRA BASIN DIRECTORY/ABRA BASIN DIRECTORY/DAILY REPORT/DAILY_RR_GIS/rainicon/NORAIN.png"
+l_rains = "C:/Users/User/Documents/KAI FILES/AbRBFFWC OBSERVER DIRECTORY/ABRA BASIN DIRECTORY/ABRA BASIN DIRECTORY/DAILY REPORT/DAILY_RR_GIS/rainicon/LGTRAINS.png"
+m_rains = "C:/Users/User/Documents/KAI FILES/AbRBFFWC OBSERVER DIRECTORY/ABRA BASIN DIRECTORY/ABRA BASIN DIRECTORY/DAILY REPORT/DAILY_RR_GIS/rainicon/MODRAINS.png"
+h_rains = "C:/Users/User/Documents/KAI FILES/AbRBFFWC OBSERVER DIRECTORY/ABRA BASIN DIRECTORY/ABRA BASIN DIRECTORY/DAILY REPORT/DAILY_RR_GIS/rainicon/HEAVYRAINS.png"
+
+
+
+
+if rr_vigan == 0:
+    vigan_rain = n_rains
+elif rr_vigan < 61:
+    vigan_rain = l_rains
+elif 60 < rr_vigan < 181:
+    vigan_rain = m_rains
+elif rr_vigan > 180:
+    vigan_rain = h_rains
+    
+if rr_bantay == 0:
+    bantay_rain = n_rains
+elif rr_bantay < 61:
+    bantay_rain = l_rains
+elif 60 < rr_bantay < 181:
+    bantay_rain = m_rains
+elif rr_bantay > 180:
+    bantay_rain = h_rains
+    
+if rr_luba == 0:
+    luba_rain = n_rains
+elif rr_luba < 61:
+    luba_rain = l_rains
+elif 60 < rr_luba < 181:
+    luba_rain = m_rains
+elif rr_luba > 180:
+    luba_rain = h_rains
+    
+if rr_dolores == 0:
+    dolores_rain = n_rains
+elif rr_dolores < 61:
+    dolores_rain = l_rains
+elif 60 < rr_dolores < 181:
+    dolores_rain = m_rains
+elif rr_dolores > 180:
+    dolores_rain = h_rains
+
+#vigan_rain = l_rains
+#bantay_rain = l_rains
+#luba_rain = l_rains
+#dolores_rain = l_rains
+
+BANTAY_WL_STAT = wl_bantay + "_BANTAY"
+DOLORES_WL_STAT = wl_dolores + "_DOLORES"
+LAPAZ_WL_STAT = wl_lapaz + "_LAPAZ"
+
+
+layout = QgsProject.instance().layoutManager().layoutByName("RG-WL_STAT")
+
+vigan_rr_img = layout.itemById("VIGAN RG")
+vigan_rr_value = layout.itemById("VIGAN_RG_AMT")
+
+vigan_rr_value.setText(str(rr_vigan) + " mm")
+vigan_rr_img.setPicturePath(vigan_rain)
+
+luba_rr_img = layout.itemById("LUBA_RG")
+luba_rr_value = layout.itemById("LUBA_RG_AMT")
+
+luba_rr_value.setText(str(rr_luba) + " mm")
+luba_rr_img.setPicturePath(luba_rain)
+
+dolores_rr_img = layout.itemById("DOLORES_RG")
+dolores_rr_value = layout.itemById("DOLORES_RG_AMT")
+
+dolores_rr_value.setText(str(rr_dolores) + " mm")
+dolores_rr_img.setPicturePath(dolores_rain)
+
+bantay_rr_img = layout.itemById("BANTAY_RG")
+bantay_rr_value = layout.itemById("BANTAY_RG_AMT")
+
+bantay_rr_value.setText(str(rr_bantay) + " mm")
+bantay_rr_img.setPicturePath(bantay_rain)
+
+
+#waterlevels
+bantay_wl_img_status = layout.itemById(BANTAY_WL_STAT)
+bantay_wl_img_status.setVisibility(1)
+
+lapaz_wl_img_status = layout.itemById(LAPAZ_WL_STAT)
+lapaz_wl_img_status.setVisibility(1)
+
+dolores_wl_img_status = layout.itemById(DOLORES_WL_STAT)
+dolores_wl_img_status.setVisibility(1)
+
+
+#base_path = os.path.join()
+pdf_path = os.path.join("C:\\Users\\User\\Documents\\KAI FILES\\AbRBFFWC OBSERVER DIRECTORY\\ABRA BASIN DIRECTORY\\ABRA BASIN DIRECTORY\\DAILY REPORT\\OUTPUTS\\WL-CHANGE\\", str(today) + "-abra_data.svg")
+
+exporter = QgsLayoutExporter(layout)
+exporter.exportToSvg(pdf_path, QgsLayoutExporter.SvgExportSettings())
